fc6f07969a1a27a21e62f0d499f0455bfc891bd8
Elin Amrén <65346494+elinamren@users.noreply.github.com>
2020-10-19 13:21:02 +0200

    added delete button to added habit, something is wrong

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 // selectors
-const habitInput = document.querySelector('.habit-input');
-const habitButton = document.querySelector('.habit-button');
-const habitList = document.querySelector('.habit-list');
+const habitInput = document.querySelector(".habit-input");
+const habitButton = document.querySelector(".habit-button");
+const habitList = document.querySelector(".habit-list");
 
 // Event listener
-habitButton.addEventListener('click', addHabit);
-
-
+habitButton.addEventListener("click", addHabit);
+habitList.addEventListener("click", deleteHabit);
 
 // functions
 function addHabit(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	// create new div
-	const habitDiv = document.createElement('div');
-		// add classlist
+  // create new div
+  const habitDiv = document.createElement("div");
+  // add classlist
 
+  // create new li
+  const newHabit = document.createElement("li");
+  // show your input on new habit row
+  newHabit.innerText = habitInput.value;
+  // add classlist
+  // append new habit inside habit div
+  habitDiv.appendChild(newHabit);
 
-	// create new li
-	const newHabit = document.createElement('li');
+  // create delete button
+  const deleteButton = document.createElement("button");
+  //add delete icon to button
+  deleteButton.innerHTML = '<i class="fas fa-times"></i>';
+  //add a class to delete button
+  deleteButton.classList.add(".delete-button");
+  //append new delete button inside habit div
+  habitDiv.appendChild(deleteButton);
 
-	// show your input on new habit row
-	newHabit.innerText = habitInput.value; 
-	
+  // append habit divs into habit list
+  habitList.appendChild(habitDiv);
 
-		// add classlist
-
-	// append new habit inside habit div
-	habitDiv.appendChild(newHabit);
-
-	// create delete button
-
-// append habit divs into habit list
-habitList.appendChild(habitDiv);
-
-// clear input field
-habitInput.value = '';
-
+  // clear input field
+  habitInput.value = "";
 }
 
-
-
-
-
-
-
-
+//delete button
+function deleteHabit(event) {
+  const item = event.target;
+  if (item.classList[0] === "delete-button") {
+    const habit = item.parentElement;
+    habit.remove();
+  }
+}
